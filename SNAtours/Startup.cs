c011a8aee8e392a3c8d19dabffc0eacb314478da
Sandy <zhangdongling@hotmail.com>
2015-03-01 08:58:26 +1100

    First commit

---
+++ SNAtours/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SNAtours.Startup))]
+namespace SNAtours
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
